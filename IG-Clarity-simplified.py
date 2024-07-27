9af97d0bddb09fff0b3af3c5a6b3a47fd563e4ce
Keerthan <kkeerthan2005@gmail.com>
2024-07-27 20:21:12 +0530

    Simplified script done. Lot better than other version released earlier

--- IG-Clarity-simplified.py
+++ IG-Clarity-simplified.py
@@ -1,22 +1,21 @@
-
-import argparse
+import os
+import sys
 import zipfile
 from bs4 import BeautifulSoup
 
-def extractor(filename):
-    with open(filename, "r") as file:
-        readfile = file.read()
 
-    soup = BeautifulSoup(readfile, 'html.parser')
+def extractor(content):
+    soup = BeautifulSoup(content, 'html.parser')
     a_elements = soup.find_all('a')
 
     people_list = []
     for a in a_elements:
         people_list.append(a.text)
-    
+
     del soup, a_elements
 
     return people_list
+
 
 def logic(path) -> list:
     """
@@ -24,24 +23,36 @@ def logic(path) -> list:
     :rtype: object
     """
     with zipfile.ZipFile(path, "r") as source:
-        source.extractall("temp_files/")
-        following = extractor("temp_files/connections/followers_and_following/following.html")
-        follower = extractor("temp_files/connections/followers_and_following/followers_1.html")
+        with source.open("connections/followers_and_following/following.html") as following_file:
+            following = extractor(following_file.read())
+        with source.open("connections/followers_and_following/followers_1.html") as follower_file:
+            follower = extractor(follower_file.read())
 
         listed = []
         for i in following:
             if i not in follower:
                 listed.append(i)
-        
-        
 
     return listed
 
-def program():
-    path_for_file = input("Enter the complete path of file: ")
 
-    print("These ppl didn't follow you back!")
-    for i in logic(path_for_file):
-        print(i)
+def program():
+    try:
+        path_for_file = sys.argv[1]
+    except:
+        print("You did not define file\nReading zip file from current directory\n")
+        try:
+            files = [file for file in os.listdir(os.getcwd()) if file.lower().endswith(".zip")]
+            path_for_file = files[0]
+        except:
+            print("Zip file does not exist in current directory?\n")
+
+    try:
+        print("These ppl didn't follow you back!")
+        for i in logic(path_for_file):
+            print(i)
+    except:
+        print("The process failed. Check the file location or ensure the zip file is in same directory as this script")
+
 
 program()
